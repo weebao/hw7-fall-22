1ffd14bfcd79744609fe2b14ad9aee788be5e49e
Bao Dang <baochidangg@gmail.com>
2022-11-18 16:42:30 -0500

    edit fileUtility

--- src/fileUtility.js
+++ src/fileUtility.js
@@ -2,9 +2,10 @@ import { readFile, writeFile } from "node:fs/promises";
 
 export function writeToJSONFile(path, data) {
   // TODO
-  return JSON.stringify(data);
+  return writeFile(path, JSON.stringify(data), { encoding })
 }
 
 export function readFromJSONFile(path) {
   // TODO
+  return readFile(path, { encoding }).then(str => JSON.parse(str))
 }
