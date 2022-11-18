bc2ee691fd4f5664ee77c0220943e1fee078a9c9
Bao Dang <baochidangg@gmail.com>
2022-11-18 15:10:10 -0500

    fetchLonAndLat

--- src/fetchLongitudeAndLatitude.js
+++ src/fetchLongitudeAndLatitude.js
@@ -1,3 +1,13 @@
+import fetch from "node-fetch";
+
 export function fetchLongitudeAndLatitude(query) {
-  // TODO
+  // Done by Bao
+  // Initialize link and append query to the link
+  const searchURL = new URL("https://geocode.maps.co/search")
+  searchURL.searchParams.append("q", query)
+  
+  // Fetch data, then 
+  return fetch(searchURL.toString())
+          .then(response => response.json())
+          .then(json => ({lon: Number(json[0].lon), lat: Number(json[0].lat)}))
 }
