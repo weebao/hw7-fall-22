0eeff716598219b63df347588cd388d25ce50ba3
Bao Dang <baochidangg@gmail.com>
2022-11-18 16:27:05 -0500

    editing main and testing around

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import fetch from "node-fetch"; // Third-party fetching library, fetch fully supported in Node.js 18+
 import path from "node:path"; // Node.js standard library for resolving arbitrary paths (like those in a url)
-// Only needed in this case to join a url given by the API to another resource
-import { fetchLongitudeAndLatitude } from "./fetchLongitudeAndLatitude.js"
 
-//testing
-fetchLongitudeAndLatitude("Amherst").then(console.log);
+import { fetchLongitudeAndLatitude } from "./fetchLongitudeAndLatitude.js"
+import { fetchCurrentWeather } from "./fetchCurrentWeather.js"
+import { fetchUniversities } from "./fetchUniversities.js"
+import { fetchUniversityWeather, fetchUMassWeather, fetchUCalWeather } from "./universityWeather.js"
+import { readFromJSONFile, writeToJSONFile } from "./fileUtility.js"
+
+//playing around
+console.log(writeToJSONFile(1, [44,4,4,4,44]))
