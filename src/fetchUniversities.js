11535687aa4578f29dec571b8afac149421a1197
Bao Dang <baochidangg@gmail.com>
2022-11-18 15:57:16 -0500

    testing

--- src/fetchUniversities.js
+++ src/fetchUniversities.js
@@ -1,10 +1,3 @@
 export function fetchUniversities(query) {
   // TODO
-  const searchURL = new URL("https://geocode.maps.co/search")
-  searchURL.searchParams.append("q", query)
-  
-  // Fetch data, then 
-  return fetch(searchURL.toString())
-          .then(response => response.json())
-          .then(json => ({lon: Number(json[0].lon), lat: Number(json[0].lat)}))
 }
