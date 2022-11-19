82ebeed9fae5395edf7d664fafedd5d5fee32c86
vkontham <vkontham1011@gmail.com>
2022-11-19 15:34:21 -0500

    function 2

--- src/fetchCurrentWeather.js
+++ src/fetchCurrentWeather.js
@@ -1,3 +1,10 @@
 export function fetchCurrentWeather(longitude, latitude) {
-  // TODO
+
+  const searchURL = new URL("https://api.open-meteo.com/v1/forecast?latitude=40&longitude=40&hourly=temperature_2m&temperature_unit=fahrenheit"); 
+
+  searchURL.searchParams.append("q", query); 
+
+  return fetch("https://api.open-meteo.com/v1/forecast?latitude=40&longitude=40&hourly=temperature_2m&temperature_unit=fahrenheit")
+  .then(response => response.json).then({time: String(json[0].time), temperature_2m: Number(json[0].temperature_2m)); 
+
 }
