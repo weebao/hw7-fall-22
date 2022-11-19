834d409d004a155d6adf2e6dcd86c6fa59722eef
Bao Dang <baochidangg@gmail.com>
2022-11-19 16:33:59 -0500

    fix first two functions

--- src/fetchLongitudeAndLatitude.js
+++ src/fetchLongitudeAndLatitude.js
@@ -1,7 +1,6 @@
 import fetch from "node-fetch";
 
 export function fetchLongitudeAndLatitude(query) {
-  // Done by Bao
   // Initialize link and append query to the link
   const searchURL = new URL("https://geocode.maps.co/search")
   searchURL.searchParams.append("q", query)
