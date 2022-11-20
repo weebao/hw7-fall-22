6ebffa3d3ee2138a309f6a208abe171aef2f3958
Bao Dang <baochidangg@gmail.com>
2022-11-19 23:17:04 -0500

    fix response.ok

--- src/fetchUniversities.js
+++ src/fetchUniversities.js
@@ -7,7 +7,7 @@ export function fetchUniversities(query) {
   
   // Fetch data, then return error if length is 0, else return list of school names
   return fetch(searchURL.toString())
-          .then(response => response.json())
+          .then(response => response.ok ? response.json() : new Error("The server is probably down."))
           .then(json => json.length === 0 ? new Error("No results found for query.")
                                           : json.map(schools => schools.name))
 }
